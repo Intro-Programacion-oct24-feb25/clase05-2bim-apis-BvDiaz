8d2a93843c3580c1cddcc22b19fcc0033c4a28d1
Bryan Diaz <vicentepomas.2007@gmail.com>
2025-01-16 11:43:35 -0500

    IMPORTAR CLASES A UN MAIN

--- Ejemplos/src/paquete03/Principal.java
+++ Ejemplos/src/paquete03/Principal.java
@@ -8,6 +8,7 @@ package paquete03;
 import paquete04.OperadorUno;
 import paquete04.OperadorDos;
 import paquete05.OperadorTres;
+import paquete06.Imprimir;
 
 // import paquete04.*;
 /**
@@ -22,11 +23,10 @@ public class Principal {
         int suma = OperadorDos.obtenerSuma(valorA, valorB);
         int multiplicacion = OperadorTres.obtenerMultiplicacion(valorA, 
                                                                 valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n"
-                + "La multiplicacion de %d * %d es igual a: %d\n",
-                valorA,valorB,
-                suma,
-                valorA, valorB,
-                multiplicacion);
+        
+        //Importanto funciones de otros paquetes a un main el cual ejecutara las funciones
+        //llamadas desde el mismo las cuales son umportadas a otro paquete mediante el
+        //api import.(nombre del paquete).(nombre de la clase).
+        Imprimir.imprimirMensaje(valorA, valorB, suma, multiplicacion);
     }
 }
